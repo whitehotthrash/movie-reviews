3599fe02c06196cb8de940b950d29d9a86be5d5a
Tim <tim.maastricht@gmail.com>
2026-02-10 15:51:21 +1000

    feat: removed cors temporarily

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import movies from './api/movies.route.js'
 
 const app = express()
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
-}))
+app.use(cors(
+//   {
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type']
+// }
+))
 app.use(express.json())
 
 app.use("/api/v1/movies", movies)
