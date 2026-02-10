16726958f884677535ebf35bb81a3e62214096f3
Tim <tim.maastricht@gmail.com>
2026-02-10 15:34:34 +1000

    fix: run locally

---
+++ backend/api/movies.route.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const router = express.Router() // get access to express router
+
+router.route('/').get((request, response) => response.send('hello world'))
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,15 @@ import movies from './api/movies.route.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}))
 app.use(express.json())
 
 app.use("/api/v1/movies", movies)
-app.use('*', (request, response) => {
+app.use('/./', (request, response) => {
   response.status(404).json({error: "not found"})
 })
 
