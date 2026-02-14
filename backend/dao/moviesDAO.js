1946065e870b44e3a2e8646ac117ef350dc12f8c
Tim <tim.maastricht@gmail.com>
2026-02-14 15:49:26 +1000

    feat: add movie retreival

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -1,6 +1,7 @@
 let movies;
 
 export default class MoviesDAO {
+  // provide database reference to movies
   static async injectDB(conn) {
     if (movies) {
       return;
@@ -9,6 +10,34 @@ export default class MoviesDAO {
       movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
     } catch (e) {
       console.error(`unable to connect in MoviesDAO: ${e}`);
+    }
+  }
+
+  static async getMovies({
+    filters = null,
+    page = 0,
+    moviesPerPage = 20,
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      }
+    }
+    let cursor;
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
     }
   }
 }
