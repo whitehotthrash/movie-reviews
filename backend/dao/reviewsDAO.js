cc838650066c75afbb57b75510e8a72fbb79aaef
Tim <tim.maastricht@gmail.com>
2026-02-28 16:32:30 +1000

    feat: wired up dao to route

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -6,6 +6,7 @@ const ObjectId = mongodb.ObjectId;
 let reviews;
 
 export default class ReviewsDAO {
+  // establish connection for routes
   static async injectDB(conn) {
     if (reviews) {
       return;
@@ -18,6 +19,23 @@ export default class ReviewsDAO {
       console.error(
         `Unable to establish connection handle in reviewsDAO: ${e}`,
       );
+    }
+  }
+
+  static async addReview(movieId, user, review, date) {
+    // add review via controller
+    try {
+      const reviewDoc = {
+        name: user.name,
+        user_id: user._id,
+        date: date,
+        review: review,
+        movie_id: ObjectId(movieId),
+      };
+      return await reviews.insertOne(reviewDoc);
+    } catch (e) {
+      console.error(`Unable to post review : ${e}`);
+      return { error: e };
     }
   }
 }
