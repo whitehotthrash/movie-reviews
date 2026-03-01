e1918a61ce531e432caee4fb98f8735821cf7b81
Tim <tim.maastricht@gmail.com>
2026-03-01 11:16:06 +1000

    feat: wired up update and delete routes to dao

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -38,4 +38,30 @@ export default class ReviewsDAO {
       return { error: e };
     }
   }
+
+  static async updateReview(reviewId, userId, review, date) {
+    // update review via controller
+    try {
+      const updateResponse = await reviews.updateOne(
+        {user_id: userId, _id:ObjectId(reviewId)},
+        {$set:{review: review, date: date}}
+      )
+      return updateResponse
+    } catch (e) {
+      console.error(`Unable to update review: ${e}`)
+    }
+  }
+
+  static async deleteReview(reviewId, userId) {
+    // delete review via controller
+    try {
+      const deleteResponse = await reviews.deleteOne({
+        _id: ObjectId(reviewId),
+        user_id: userId,
+      })
+    } catch (e) {
+      console.error(`Unable to delete review: ${e}`)
+      return { error: e }
+    }
+  }
 }
