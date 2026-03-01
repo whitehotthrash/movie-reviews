2dd0ea8a915bfbf7781f3b04debd502cae0e3e54
Tim <tim.maastricht@gmail.com>
2026-03-01 11:28:18 +1000

    fix: delete function

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -59,6 +59,7 @@ export default class ReviewsDAO {
         _id: ObjectId(reviewId),
         user_id: userId,
       });
+      return deleteResponse
     } catch (e) {
       console.error(`Unable to delete review: ${e}`);
       return { error: e };
