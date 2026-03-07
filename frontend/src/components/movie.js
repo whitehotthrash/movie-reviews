e7ed4b63ca215207a08fe2517138e769212e5560
Tim <tim.maastricht@gmail.com>
2026-03-07 17:13:25 +1000

    feat: add movie component

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,7 +1,30 @@
 import { useEffect, useState } from "react";
+import MovieDataService from '../services/movies'
+import { Link } from 'react-router-dom'
 
 const Movie = (props) => {
-  return <div className="App">Movie</div>;
+
+  const [movie, setMovie] = useState({
+    id: null,
+    title: "",
+    rated: "",
+    reviews: []
+  })
+
+  const getMovie = id => {
+    MovieDataService.get(id).then(response => {
+      setMovie(response.data)
+      console.log(response.data)
+    }).catch(e => {
+      console.log(e)
+    })
+  }
+
+  useEffect(() => {
+    getMovie(props.match.params.id)
+  }, [props.match.params.id])
+
+  return <div></div>;
 };
 
 export default Movie;
