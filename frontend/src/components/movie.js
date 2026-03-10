c43b00d75471ad8cfe1ba46d47a819beab161ff9
Tim <tim.maastricht@gmail.com>
2026-03-10 18:38:42 +1000

    feat: add single movie front end

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,30 +1,61 @@
 import { useEffect, useState } from "react";
-import MovieDataService from '../services/movies'
-import { Link } from 'react-router-dom'
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Media from "react-bootstrap/Media";
 
 const Movie = (props) => {
-
   const [movie, setMovie] = useState({
     id: null,
     title: "",
     rated: "",
-    reviews: []
-  })
+    reviews: [],
+  });
 
-  const getMovie = id => {
-    MovieDataService.get(id).then(response => {
-      setMovie(response.data)
-      console.log(response.data)
-    }).catch(e => {
-      console.log(e)
-    })
-  }
+  const getMovie = (id) => {
+    MovieDataService.get(id)
+      .then((response) => {
+        setMovie(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   useEffect(() => {
-    getMovie(props.match.params.id)
-  }, [props.match.params.id])
+    getMovie(props.match.params.id);
+  }, [props.match.params.id]);
 
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Image src={movie.poster + "/100x250"} fluid />
+          </Col>
+          <Card>
+            <Card.Header as="h5">{movie.title}</Card.Header>
+            <Card.Body>
+              <Card.Text>{movie.plot}</Card.Text>
+              {props.user && (
+                <Link to={"/movies/" + props.match.params.id + "/review"}>
+                  Add Review
+                </Link>
+              )}
+            </Card.Body>
+          </Card>
+          <br></br>
+          <h2>Reviews</h2>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 // TODO: find a better way to export
 export default Movie;
